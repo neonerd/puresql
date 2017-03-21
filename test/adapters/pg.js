25bf0615108074cc82cc1a8ec74a8cd68b85ce29
Andrej Sykora <as@andrejsykora.com>
2017-03-21 12:05:17 +0100

    rename test

---
+++ test/adapters/pg.js
@@ -0,0 +1,60 @@
+/* global it, describe */
+'use strict'
+
+// test stuff
+var chai = require('chai')
+var chaiAsPromised = require('chai-as-promised')
+chai.use(chaiAsPromised)
+
+var expect = chai.expect
+
+var pg = require('pg')
+
+// exported library
+var puresql = require('../../index')
+
+// config
+// defaults to travis, use test/config.js file to export your own
+var config = {
+  pg: {
+    host: '127.0.0.1',
+    user: 'postgres',
+    password: '',
+    database: 'puresql_test'
+  }
+}
+var localConfig
+try {
+  localConfig = require('../config')
+} catch (error) {
+}
+if (localConfig !== undefined) config = localConfig
+
+// -- TESTS
+
+// LIBRARY
+describe('puresql.adapters.pg', () => {
+  // create a connection the adapter will use
+  var connection = new pg.Client(config.pg)
+  var adapter = puresql.adapters.pg(connection)
+
+  it('should escape a string correctly', () => {
+    expect(adapter.escape('john')).to.equal("'john'")
+    expect(adapter.escape(1)).to.equal(1)
+    expect(adapter.escape("john' OR 'michael'")).to.equal("'john'' OR ''michael'''")
+  })
+
+  it('should process a query correctly', (done) => {
+    connection.connect((err) => {
+      adapter.query('SELECT 1 AS foo')
+      .then((rows) => {
+        expect(rows[0]['foo']).to.equal(1)
+        done()
+      })
+    })
+  })
+
+  it('should throw a query error correctly', () => {
+    expect(adapter.query('SELECT 1 A foo')).to.be.rejected
+  })
+})
